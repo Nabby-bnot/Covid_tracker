b6c8fe73a65e3cc1c3ef4146e0a2829c0e466b9b
Nabhendu Das <nabhey123@gmail.com>
2021-06-29 16:06:50 +0530

    commit

---
+++ covid_tracker/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Cards, Chart, CountryPicker } from "./Components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+import coronaImage from "./images/image.jpg";
+
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+    this.setState({ data: fetchedData, country: country });
+  };
+  render() {
+    const { data, country } = this.state;
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <br />
+        <text>
+          <h1 style={{fontFamily: 'Roboto'}}>COVID-19 TRACKER</h1>
+        </text>
+        <br />
+        <text>
+          <p>Be safe and be updated about you country!</p>
+        </text>
+        <br />
+        <br />
+        <div className={styles.line}></div>
+        <Cards data={data} country={country} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
